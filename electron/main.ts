80bc8fa655a70fe9a8ee656bd62a3821ccd9cc8d
Pulkit Singh <pulkit.singh@shipsy.co.in>
2020-11-01 16:37:20 +0530

    .

--- electron/main.ts
+++ electron/main.ts
@@ -5,14 +5,15 @@ import installExtension, { REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } from 'electro
 
 let mainWindow: Electron.BrowserWindow | null
 
-function createWindow() {
+function createWindow () {
   mainWindow = new BrowserWindow({
     width: 1100,
     height: 700,
     backgroundColor: '#191622',
     webPreferences: {
       nodeIntegration: true
-    }
+    },
+    frame: false
   })
 
   if (process.env.NODE_ENV === 'development') {
@@ -38,10 +39,10 @@ app.on('ready', createWindow)
     if (process.env.NODE_ENV === 'development') {
       installExtension(REACT_DEVELOPER_TOOLS)
         .then((name) => console.log(`Added Extension:  ${name}`))
-        .catch((err) => console.log('An error occurred: ', err));
+        .catch((err) => console.log('An error occurred: ', err))
       installExtension(REDUX_DEVTOOLS)
         .then((name) => console.log(`Added Extension:  ${name}`))
-        .catch((err) => console.log('An error occurred: ', err));
+        .catch((err) => console.log('An error occurred: ', err))
     }
   })
 app.allowRendererProcessReuse = true
