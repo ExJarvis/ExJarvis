8d82995c4b037bb8c5645c3cbb7f0f6966305cf2
Pulkit Singh <pulkit.singh@shipsy.co.in>
2020-11-20 13:38:06 +0530

    scraping | .

--- electron/spider.ts
+++ electron/spider.ts
@@ -1,4 +1,51 @@
-import puppeteer from 'puppeteer';
-import { getChromePath } from './utils';
+import puppeteer, { Page } from 'puppeteer-core';
 
-puppeteer.launch({executablePath: getChromePath()});
+const getBrowser = async () => {
+  const browser = await puppeteer.launch({
+    executablePath: require('chrome-location'),
+    // devtools: true,
+    // headless: false,
+  });
+  return browser;
+};
+
+export const scrape = async () => {
+  const browser = await getBrowser();
+  const page = await browser.newPage();
+  await page.goto('https://www.google.com');
+  await page.screenshot({ path: '/Users/pulkitsingh/dev/chaakar/dist/example.png' });
+
+  await browser.close();
+};
+
+const log = async (page: Page) => {
+  page.on('console', (msg) => console.log('PAGE LOG:', msg.text()));
+  await page.evaluate(() => console.log(`url is ${location.href}`));
+};
+
+export const search = async () => {
+  const browser = await getBrowser();
+  const page = await browser.newPage();
+  await page.goto('https://duckduckgo.com/?q=sdf', { waitUntil: 'networkidle2' });
+  // await page.type('#search_form_input_homepage', 'Puppeteer');
+  // await page.click('#search_button_homepage');
+  const selector = '//*/div/h2';
+  await page.waitForXPath(selector);
+  const searchValue = await page.$x(selector);
+  await page.screenshot({ path: '/Users/pulkitsingh/dev/chaakar/dist/example.png' });
+  searchValue.map(async (element) => {
+    console.log(await page.evaluate((el) => el.textContent, element));
+  });
+  log(page);
+  // await browser.close();
+};
+
+const main = () => {
+  try {
+    search();
+  } catch (e) {
+    console.log({ e });
+  }
+};
+
+main();
